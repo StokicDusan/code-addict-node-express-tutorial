b4beefbbf0999005cf8e45699a02bad9d036df5b
StokicDusan <stokicdusan@gmail.com>
2021-12-30 22:19:25 +0100

    settup some console log-s

--- myNode/tutorial/10-fs-sync-module.js
+++ myNode/tutorial/10-fs-sync-module.js
@@ -1,5 +1,6 @@
 // synchronous -> blocking
 const {readFileSync,writeFileSync} = require('fs');
+console.log('start')
 
 const first = readFileSync('./content/first.txt','utf-8')
 const second = readFileSync('./content/second.txt','utf-8')
@@ -12,4 +13,5 @@ writeFileSync(
     {flag: 'a'}                                 // append
 )
 
-
+console.log('done with this task')
+console.log('starting the next task')
