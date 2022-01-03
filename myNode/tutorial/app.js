0ffbacd496eca8aa36879b7090ea458ce2017b9c
StokicDusan <stokicdusan@gmail.com>
2022-01-03 11:18:19 +0100

    minor update

--- myNode/tutorial/app.js
+++ myNode/tutorial/app.js
@@ -1,1 +1,23 @@
-console.log('Hello, to Node Tutorial')
+console.log('Hello and welcome to Node Tutorial')
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Home Page')
+  }
+  if (req.url === '/about') {
+    // blocking code
+    for (let i = 0; i < 300; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`)
+      }
+    }
+    res.end('About Page')
+  }
+  res.end('Error Page')
+})
+
+server.listen(5000, () => {
+  console.log('Server listening on port : 5000....')
+})
