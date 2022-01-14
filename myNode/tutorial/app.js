54008b9247db9a22778c6f6d9b2f975fc8253d1d
StokicDusan <stokicdusan@gmail.com>
2022-01-14 12:40:45 +0100

    minor update

--- myNode/tutorial/app.js
+++ myNode/tutorial/app.js
@@ -1,1 +1,19 @@
 console.log('Welcome to Node Tutorial')
+
+const {createReadStream} = require('fs');
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+
+const stream = createReadStream('./content/big.txt');
+
+stream.on('data',(result)=>{
+    console.log(result);
+    console.log(result.length);
+    //we are reading in chunks now !!
+})
+
+stream.on('error',(err)=> console.log(err))
