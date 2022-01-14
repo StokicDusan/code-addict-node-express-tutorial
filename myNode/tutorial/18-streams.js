c93f35f7e30a5911ba62181526a2b61b5e391f8b
StokicDusan <stokicdusan@gmail.com>
2022-01-15 00:40:28 +0100

    initial streams file

---
+++ myNode/tutorial/18-streams.js
@@ -0,0 +1,12 @@
+//Streams - Writable, Readable, Duplex, Transform
+
+// Just like with events, many built in modules in node 
+// implement streaming interface
+
+// Streams extend event emiters class !!
+
+// Read stream option helps with files that are to big to read
+// or if we want the conserve memory because we might not need 
+// everything from that, lets say, text file
+
+
